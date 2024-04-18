a7aca97b5a47d8c01f85607ab69cf43a2ebb5b43
Austen Boodell <eb115@cs.wellesley.edu>
2024-04-17 20:30:10 -0400

    what we submitted

--- server.js
+++ server.js
@@ -53,19 +53,19 @@ const STAFF = 'staff';
 const MOVIES = 'movies';
 const PEOPLE = 'people';
 
+// renders home page
 app.get("/", (req, res) => {
-    // examples of flashing
-    req.flash('error', 'no error yet!'); 
-    req.flash('info', 'hello there!'); 
     return res.render("index.ejs");
 });
 
+// renders insert page with form to insert movie
 app.get("/insert/", (req, res) => {
     return res.render("insert.ejs")
 })
 
+// inserts movie into database with given information
 app.post("/insert/", async(req, res) => {
-    const id = req.body.movieTt;
+    const id = parseInt(req.body.movieTt);
     console.log("tt in insert", id)
     if (!id) {
         req.flash("info", "please enter a movie id")
@@ -99,7 +99,7 @@ app.get("/search/", (req, res) => {
     //allowed to skip this feature
 });
 
-
+// renders select incomplete movies page with all movies without either a release or director
 app.get("/select/", async (req, res) => {
     const db = await Connection.open(mongoUri, "am114");
     const movies = db.collection("movies");
@@ -109,23 +109,19 @@ app.get("/select/", async (req, res) => {
     return res.render("select.ejs", {movie: movie}) //need to render the results
 });
 
+// renders update page for selected movie
 app.get("/do-select/", async (req, res) => {
     const movieID = req.query.menuTt;
     console.log("movieTt", movieID)
     const db = await Connection.open(mongoUri, "am114");
     const movies = db.collection("movies");
-    let movie = await movies.find({tt: movieID}).toArray();
-    if (movie.length>0) {
-        console.log("should be rendering")
-        res.redirect(`/update/` + movie[0].tt);
-    } else {
-         movie = await movies.find().toArray();
-        console.log("should be flashing", movie)
-        req.flash("info", `please select a different movie`); //todo - this isnt working
-        //maybe should rerender page?
-    }
+    let movie = await movies.find({tt: parseInt(movieID)}).toArray();
+    movie = movie[0];
+    console.log("should be rendering", movie)
+    return res.render("update.ejs", {url: "/update/" + movieID, tt: movieID, title: movie.title, release: movie?.release, addedBy: movie?.addedby?.name, addedById: movie?.addedby?.uid, directorId: movie?.director?.nm, director: movie?.director?.name})
 });
 
+// renders update page for given movie id
 app.get("/update/:tt", async(req,res) => {
     const movieID = req.params.tt;
     console.log(movieID)
@@ -133,34 +129,49 @@ app.get("/update/:tt", async(req,res) => {
     const movies = db.collection("movies");
     let movie = await movies.find({tt: movieID}).toArray();
     console.log("movie", movie)
-    return res.render("update.ejs", {url: "update" + movieID, tt: movieID, title: movie[0]?.title, release: movie[0]?.release, addedBy: movie[0]?.addedby?.name, addedById: movie[0]?.addedby?.nm, directorId: movie[0]?.director?.nm, director: movie[0]?.director?.name})
+    return res.render("update.ejs", {url: "/update/" + movieID, tt: movieID, title: movie[0]?.title, release: movie[0]?.release, addedBy: movie[0]?.addedby?.name, addedById: movie[0]?.addedby?.nm, directorId: movie[0]?.director?.nm, director: movie[0]?.director?.name})
 })
 
+// updates movie database for movie of given id
 app.post("/update/:tt", async(req,res) => {
-    const id = req.params.tt;
+    const id = parseInt(req.params.tt);
     const movieTitle = req.body.movieTitle;
-    const releaseYear = req.body.movieRelease;
-    const movieAddedBy = req.body.movieAddedBy;
-    const directorId = req.body.movieDirectorid;
+    const releaseYear = parseInt(req.body.movieRelease) || "";
+    const movieAddedBy = parseInt(req.body.movieAddedby) || "";
+    const directorId = parseInt(req.body.movieDirectorid) || "";
     const db = await Connection.open(mongoUri, "am114");
-    const people = db.collection("people"); //todo - we need to finish this
-    const lookUpDirector = await people.find({nm: directorId}, {nm: 1, name: 1});
-    const lookUpAddedBy = await people.find({nm: movieAddedBy}, {nm: 1, name: 1});
+    const people = db.collection("people"); 
+    console.log("directorID", directorId)
+    const lookUpDirector = await people.find({nm: directorId}, {nm: 1, name: 1}).toArray();
+    console.log("director", lookUpDirector[0])
+    const lookUpAddedBy = await people.find({nm: movieAddedBy}, {nm: 1, name: 1}).toArray();
     const movieObject = {
         tt: id,
         title: movieTitle,
         release: releaseYear,
-        director: lookUpDirector,
-        addedby: lookUpAddedBy,
+        director: lookUpDirector[0],
+        addedby: lookUpAddedBy[0],
     }
+    console.log("movie object", movieObject)
     const movies = db.collection("movies");
     await movies.updateOne({tt: id}, { $set: movieObject});
-    return res.render("update.ejs", {url: "update/" + id, tt: id, title: movieTitle, release: releaseYear, addedBy: lookUpAddedBy?.name, addedById: lookUpAddedBy?.nm, directorId: lookUpDirector?.nm, director: lookUpDirector?.name})
+    console.log("release", releaseYear)
+    return res.render("update.ejs", {url: "/update/" + id, tt: id, title: movieTitle, release: releaseYear, addedBy: lookUpAddedBy[0]?.name, addedById: lookUpAddedBy[0]?.nm, directorId: lookUpDirector[0]?.nm, director: lookUpDirector[0]?.name})
 
 })
 
+// deletes movie with given id from the database
+app.post("/delete/:tt", async(req,res) => {
+    const id = parseInt(req.params.tt);
+    const db = await Connection.open(mongoUri, "am114");
+    const movies = db.collection("movies");
+    await movies.deleteOne({tt: id});
+    req.flash("info", `you sucessfully deleted movie id(${id})`)
+    return res.redirect("/")
+})
+
 // ================================================================
-// postlude
+// postlude0
 
 const serverPort = cs304.getPort(8080);
 
